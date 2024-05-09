090f58dbf948ce697d222c7ba3a6936e6a246233
myFirstGitDemoProject <nagesh@aven-sys.com>
2024-05-09 08:12:37 +0530

    changes commit

--- src/app/Pipes/search.pipe.ts
+++ src/app/Pipes/search.pipe.ts
@@ -7,7 +7,7 @@ export class SearchPipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
     if(!value) return null;
-    if(!args) return value;
+    if(!args) return value; 
     args=args.toLowerCase();
     return value.filter((item:any)=>{
       return JSON.stringify(item).toLowerCase().includes(args);
